40e31f7d3ddea5afbf60b1acc1c46968a8cb8491
Sebastian Pineda <camilopineda77@hotmail.com>
2014-06-13 13:37:50 -0500

    tutorial terminado

---
+++ ex13.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your firs variable is: ", first
+print "your second variable is: ", second
+print "Your third variable is: ", third
+
+# para ejecutar este archivo toca pasar cuantro parametros en total
+# de la siguiente manera: python ex13.py <secondo> <third> <fouth>
+ 
---
+++ ex9.py
@@ -0,0 +1,14 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+with the three double.quotes.
+we'll be able to type as much as we like.
+even 4 lines if we want, or 5, or 6.
+"""
